447e503faaa49a717fd26bcc4d3d67ce418d6c79
Reece H. Dunn <msclrhd@gmail.com>
2015-04-27 10:06:48 +0100

    Link to the automake files needed to run the configure script.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ Add autotools support:
 
   * Added an autogen.sh script to setup the configure script.
   * Backport configure.in from 1.4.2 as configure.ac.
+  * Link to the automake files needed to run the configure script.
   * Use `autoconf` to generate the configure script.
 
 ## [1.4.1]
--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
+if [ x${AUTOMAKE_DIR} = x ] ; then
+	AUTOMAKE_DIR=`find /usr/share/automake* -maxdepth 0 -type d 2>&1 | sort | tail -n 1`
+fi
+
+if [ ! -e "${AUTOMAKE_DIR}" -o ! -e "${AUTOMAKE_DIR}/install-sh" ] ; then
+	echo "Unable to locate the automake directory."
+	echo "Set AUTOMAKE_DIR to the correct automake directory path."
+	exit 1
+fi
+
+ln -sf ${AUTOMAKE_DIR}/config.guess .
+ln -sf ${AUTOMAKE_DIR}/config.sub .
+
+ln -sf ${AUTOMAKE_DIR}/install-sh .
+ln -sf ${AUTOMAKE_DIR}/missing .
+ln -sf ${AUTOMAKE_DIR}/mkinstalldirs .
+
 autoconf
